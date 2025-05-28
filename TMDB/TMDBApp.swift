7a6b8ee2366b487bc25c93dbc29c1855eec967d8
Momeks <momeksdev@gmail.com>
2025-05-28 10:10:26 +0200

    Initial Commit

---
+++ TMDB/TMDBApp.swift
@@ -0,0 +1,17 @@
+//
+//  TMDBApp.swift
+//  TMDB
+//
+//  Created by Mohammad Komeili on 28.05.25.
+//
+
+import SwiftUI
+
+@main
+struct TMDBApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
